88e304cbd336b78414a74d834c4f6f94f720f6ec
Glory96 <44321800+Glory96@users.noreply.github.com>
2018-11-11 22:05:55 +0300

    Add files via upload

---
+++ Assignment2/behave.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    var x = document.forms["Assignment2.html"]["name"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+	
+}
+
+
+
+
